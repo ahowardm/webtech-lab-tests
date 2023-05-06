7d387002752a8fc36bff488eeccd3b384e8808d0
Andrés Howard <ahowardm@gmail.com>
2023-05-06 11:09:12 -0400

    feat: new version

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,4 +1,4 @@
-module WebTech::Lab::Tests
+module WebTech
     module Generators
         class InstallGenerator < Rails::Generators::Base
             source_root File.expand_path('templates', __dir__)
--- lib/webtech/lab/tests/version.rb
+++ lib/webtech/lab/tests/version.rb
@@ -1,7 +1,7 @@
 module Webtech
   module Lab
     module Tests
-      VERSION = "0.1.0"
+      VERSION = "0.1.1"
     end
   end
 end
