a2a5f416e74708a5b4e03255531f48413afb3989
Andrés Howard <ahowardm@gmail.com>
2023-05-06 10:57:59 -0400

    fix: remove TODOs from specfile

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,7 +1,11 @@
-class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('templates', __dir__)
-    
-    def copy_model_tests
-        copy_file 'fixtures/categories.yml'
+module WebTech::Lab::Tests
+    module Generators
+        class InstallGenerator < Rails::Generators::Base
+            source_root File.expand_path('templates', __dir__)
+            
+            def copy_model_tests
+                copy_file 'fixtures/categories.yml'
+            end
+        end
     end
 end
